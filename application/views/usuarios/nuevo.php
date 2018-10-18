0c342d89f7c0de0fce1d922fa842c1335f88c946
Brando <BrandoLarios@outlook.com>
2018-10-17 21:27:27 -0500

    Añadida la parte de usuario

---
+++ application/views/usuarios/nuevo.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+    <!--<link rel="stylesheet" type="text/css" href="<?php //echo base_url('assets/css/bootstrap.min.css.map'); ?>">-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+
+</head>
+<body>
+    
+    <div class="container">
+        <h2>Agregar usuario</h2><br>
+        <form action="<?= base_url('') ?>" method="POST">  
+        <!-- Inicio de información personal -->
+            <h5 class="card-title" for="">Información personales</h5>
+            <!-- Nombre, apellido y CURP -->
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Nombre:</span>
+                        </div>
+                        <input class="form-control" type="text" id="" name="name" placeholder="Primer nombre" required value="">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Apellido:</span>
+                        </div>
+                        <input class="form-control" type="text" id="" name="lastname" placeholder="Primer apellido" required>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">CURP:</span>
+                        </div>
+                        <input class="form-control" type="text" id="" name="CURP" placeholder="Ingrese 18 caracteres" required>
+                    </div>
+                </div>
+            </div><br>
+            <!-- Correo, numero de telefono y celular -->
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Correo:</span>
+                        </div>
+                        <input class="form-control" type="email" id="" name="email" placeholder="ejemplo@ejem.plo" required>
+                    </div>
+                </div> 
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Telefono:</span>
+                        </div>
+                        <input class="form-control" type="tel" id="" name="phone" placeholder="5558855" pattern="[0-9]{7}">
+                    </div> 
+                </div>          
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Celular:</span>
+                        </div>
+                        <input class="form-control" type="tel" id="" name="cellphone" placeholder="5558885588" pattern="[0-9]{10}" required>
+                    </div>
+                </div>
+            </div><br>
+        <!-- Fin de información personal -->
+
+        <!-- Inicio de información de residencial -->
+            <!-- Dirección, colonia y municipio -->
+            <h5 class="card-title" for="">Información residencial</h5>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Dirección:</span>
+                        </div>
+                        <input class="form-control" type="text" id="" name="direction" placeholder="Calle y número" required>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Colonia:</span>
+                        </div>
+                        <input class="form-control" type="text" id="" name="colony" placeholder="No abrevie el nombre">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Municipio:</span>
+                        </div>
+                        <input class="form-control" type="text" id="" name="municipality" placeholder="No abrevie el nombre" required>
+                    </div>
+                </div>
+            </div><br>
+            <!-- Estado y codigo postal -->
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Estado:</span>
+                        </div>
+                        <input class="form-control" type="text" id="" name="state" placeholder="No abrevie el nombre" required>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Código postal:</span>
+                        </div>
+                        <input class="form-control" type="number" id="" name="cp" placeholder="58585" min="00000" max="99999" pattern="[0-9]{5}" required>
+                    </div>
+                </div>
+            </div><br>
+        <!-- Fin de información de residencial -->
+
+        <!-- Inicio de información laboral -->
+            <h5 class="card-title" for="">Información laboral</h5>
+            <!-- RFC y fecha de contratación -->
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">RFC:</span>
+                        </div>
+                        <input class="form-control" type="text" id="" name="RFC" required>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Fecha de contratación:</span>
+                        </div>
+                        <input class="form-control" type="date" id="" name="contract_date" min="1960-01-01" value="<?php echo date('Y-m-d');?>" required>
+                    </div> 
+                </div>
+                <div class="col-md-4">
+                </div>
+            </div><br>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Hora de entrada:</span>
+                        </div>
+                        <input class="form-control" type="time" id="" name="inhour" required>
+                    </div> 
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Hora de salida:</span>
+                        </div>
+                        <input class="form-control" type="time" id="" name="outhour" required>
+                    </div> 
+                </div>
+                <div class="col-md-4">
+                </div>
+            </div><br>
+        <!-- Inicio de información laboral -->
+
+            <!-- Botones para regresar a home y agregar al ususario -->
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <button class="btn btn-success float-right" type="submit">
+                        Agregar <i class="fa fa-save"></i>
+                    </button>
+                </div>
+            </div> 
+        </form>
+    </div>
+
+</body>
+</html>
